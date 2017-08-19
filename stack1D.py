2dbf10c2727745dc39a3de884207c2da822c3b50
jb <jb.jolly@yahoo.fr>
2017-08-19 16:47:26 +0200

    added MCMC

--- stack1D.py
+++ stack1D.py
@@ -18,6 +18,8 @@ class Image():
         self.numberOfLines=numberOfLines
         self.spectrum=spectrum
         self.lines=lines
+    def setLines(self, newLines):
+        self.lines=newLines
 
 def Spectrum(frequencies, lines=[], noise=False, sigmaNoise=0):
     spectrum=np.zeros(len(frequencies))
@@ -57,6 +59,9 @@ class Line():
         self.fToday=self.fEm/(1+self.z)
         self.observedFToday=self.fEm/(1+self.observedZ)
 
+    def setZ(self, newZ):
+        self.observedZ=newZ
+
 def Gauss(  f=[], #this should be a numpy array
             f0=0, #center of your gaussian
             dV='default', #velocity, used to calculate frequency width, set to 'default' to avoid errors
@@ -77,6 +82,8 @@ def Gauss(  f=[], #this should be a numpy array
 /&\ STACKSIZE HAS TO BE AN ODD NUMBER
 '''
 def Stack(images, method='mean', stackSize=0, visu=0):
+    if stackSize/2.==int(stackSize/2.):
+        raise Exception('STACKSIZE IS AN EVEN NUMBER')
     toStack=np.zeros((len(images), stackSize))
     for i in range(len(images)):
         thisImage=images[i]
@@ -95,6 +102,46 @@ def Stack(images, method='mean', stackSize=0, visu=0):
         check median fct
         '''
         return np.median(toStack,0)
+
+
+def MCMC_once(oldSummedStack=0, oldImages=[], deltaZ=0, sumWidth=0, method='mean'):
+    #newZ=np.zeros((numberOfImages, numberOfLinesPerImage))
+    newImages=([0 for i in range(numberOfImages)])
+    for i,image in enumerate(oldImages):
+        tempLines=[]
+        for j, line in enumerate(image.lines):
+            newZ=np.random.normal(line.observedZ, deltaZ)
+            tempLine=line
+            tempLine.setZ(newZ)
+            tempLines.append(tempLine)
+        newImages[i]=image
+        newImages[i].setLines(tempLines)
+
+    newSummedStack=np.mean(Stack(newImages, method, stackSize=sumWidth))
+
+    if newSummedStack>oldSummedStack:
+        return newSummedStack, newImages
+    else:
+        return oldSummedStack, oldImages
+
+def MCMC(nRandom=1, oldStack=0, oldImages=[], deltaZ=0, sumWidth=0, method='mean', saveResult=True):
+    theseImages=oldImages
+    thisSummedStack=oldSummedStack
+    if saveResult:
+        result=([ 0 for i in range(nRandom) ])
+        for i in range(nRandom):
+            print 'mcmc', i
+            result[i]=MCMC_once(oldSummedStack=thisSummedStack, oldImages=theseImages, deltaZ=deltaZ, sumWidth=sumWidth, method=method)
+            thisStack=result[i][0]
+            theseImages=result[i][1]
+    else:
+        for i in range(nRandom):
+            print 'mcmc', i
+            result=MCMC_once(oldSummedStack=thisSummedStack, oldImages=theseImages, deltaZ=deltaZ, sumWidth=sumWidth, method=method)
+            thisStack=result[0]
+            theseImages=result[1]
+
+    return result
 
 ''' ================ D E F I N I T I O N S ================== '''
 
@@ -117,11 +164,11 @@ freqMaxLim=myFmax-2*panWidth
 freqMinLim=myFmin+2*panWidth
 zRange_pan=[(emittedFrequency/freqMaxLim)-1,(emittedFrequency/freqMinLim)-1]
 myNoiseAmp=lineMeanAmp/2
-myNoise=True
+myNoise=False
 
 ''' ======= I M A G E S    G E N E R A T I O N   ======= '''
 
-
+print 'generating images'
 allImages=[]
 for i in range(numberOfImages):
     print i
@@ -145,13 +192,56 @@ for i in range(numberOfImages):
                                                 sigmaNoise=myNoiseAmp),#spectrum=Spectrum(fullFrequencies, lines=thisImageLines),
                             lines=thisImageLines))
 
-
+print 'done'
 ''' =========== S T A C K ============= '''
-
+print 'starting to stack'
 stacked=Stack(allImages, stackSize=numberOfChansStack)
 
+print 'done'
+''' ============= M C M C =========== '''
+print 'starting to do some sweet MCMC'
 
-''' ======== B O O T S T R A P ======== '''
+
+sumWidth=int(numberOfChansStack/2)+1
+firstStackMCMC=Stack(allImages, stackSize=sumWidth)
+
+nRandom=100
+oldSummedStack=np.mean(firstStackMCMC)
+oldImages=allImages
+deltaZ=0.01
+method='mean'
+saveResult=True
+
+allMCMCResults=MCMC(    nRandom=nRandom,
+                        oldStack=oldSummedStack,
+                        oldImages=oldImages,
+                        deltaZ=deltaZ,
+                        sumWidth=sumWidth,
+                        saveResult=saveResult)
+print 'done'
+
+
+
+import matplotlib.pyplot as plt
+import math
+
+fig=plt.figure()
+ax=([fig.add_subplot(3,math.ceil(numberOfImages/3),i+1) for i in range(len(allMCMCResults[-1][1])) ])
+
+for i in range(len(allMCMCResults[-1][1])):
+    print i
+
+for i in range(len(allMCMCResults[-1][1])):
+    print i
+    '''/&\ IL FAUT MODIF lines[0] par une for loop lines[n]'''
+
+    ax[i].plot(([allMCMCResults[p][1][i].lines[0].z for p in range(len(allMCMCResults))]))
+
+    ax[i].plot( ([ allMCMCResults[j][1][i].lines[0].observedZ for j in range(len(allMCMCResults)) ]) )
+
+fig.show()
+
+''' ======== B O O T S T R A P ========
 stackedBoot=([0 for i in range(numberOfImages) ])
 
 stackedBoot1=([0 for i in range(numberOfImages) ])
@@ -187,13 +277,13 @@ import math
 #fig3=plt.figure()
 #ax=([fig3.add_subplot(3,math.ceil(numberOfImages/3),i+1) for i in range(len(leVecteurDeLaDiff1)) ])
 toplot=([ [np.mean(leVecteurDeLaDiff[i][9:12]), np.mean(leVecteurDeLaDiff1[i][9:12])] for i in range(len(leVecteurDeLaDiff)) ])
-'''
+
 for i in range(len(toplot)):
         ax[i].plot(toplot[i])
         ax[i].set_title(str(i))
 plt.savefig('letest.pdf')
 fig3.show()
-'''
+
 for i in range(len(toplot)):
     if i==0:
         plt.plot(toplot[i], linewidth=10)
@@ -204,3 +294,4 @@ print '+1'
 print np.mean(leVecteurDeLaDiff1,1)
 print ''
 print np.mean(leVecteurDeLaDiff1,0)
+'''
